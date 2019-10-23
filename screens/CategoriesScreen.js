c8b6cefa15d733c7ccb9905fe10606af6a15c24d
Jascuas <javiersanchezdelago@hotmail.com>
2019-10-23 13:31:28 +0200

    Adding dummy data for categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
-const CategoriesScreen = props => {
-
+const renderGridItem = itemdata => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button title="Go to meals!" onPress={() => {
-        props.navigation.navigate({routeName: "CategoryMeals"})
-      }}/>
+    <View style={styles.gridItem}>
+      <Text>{itemdata.item.title}</Text>
     </View>
+  );
+};
+const CategoriesScreen = props => {
+  return (
+    <FlatList
+      numColumns={2}
+      renderItem={renderGridItem}
+      data={CATEGORIES}
+      keyExtractor={(item, index) => item.id}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  gridItem: {
+    flex:1,
+    margin: 15,
+    height: 150
   }
 });
 
